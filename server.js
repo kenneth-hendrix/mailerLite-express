4c1e6fc2733f0f59b919733e6a1d81c6d0ed9c0b
Kenneth Hendrix <kennethhendrix72@gmail.com>
2024-10-26 22:10:54 -0400

    Update send

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 const API_KEY = process.env.MAILERLITE_API_KEY;
 const GROUP_ID = process.env.GROUP_ID;
 const USER_ID = process.env.USER_ID;
+const CAMPAIGN_ID = process.env.CAMPAIGN_ID;
 
 const mailerlite = new MailerLite({
   api_key: API_KEY,
@@ -55,57 +56,30 @@ app.post("/api/subscriber", async (req, res) => {
 });
 
 app.post("/api/sendMail", async (req, res) => {
+  const { user } = req.body;
+  if (user !== USER_ID) {
+    res.status(500).json({ error: "Unauthorized User" });
+    return;
+  }
+
   const params = {
-    name: "New Tate Take",
-    type: "regular",
-    emails: [
-      {
-        subject: "New Tate Take",
-        from_name: "Tate Takes",
-        from: "tate@tatetakes.com",
-        content:
-          '<!DOCTYPE html>\n<html>\n<head>\n  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n  <meta name="viewport" content="width=device-width, initial-scale=1.0" />\n  <meta http-equiv="X-UA-Compatible" content="IE=edge" />\n  <meta name="format-detection" content="address=no" />\n  <meta name="format-detection" content="telephone=no" />\n  <meta name="format-detection" content="email=no" />\n  <meta name="x-apple-disable-message-reformatting" />\n  <title>Untitled</title>\n  <!-- Style goes here -->\n  <style type="text/css">\n\n  </style>\n</head>\n<body style="margin: 0; padding: 0;">\n<!-- Main table -->\n<table border="0" cellspacing="0" cellpadding="0" width="100%">\n  <tr>\n    <td style="padding: 0 40px;">\n      <!-- Child table -->\n      <table align="center" border="0" cellspacing="0" cellpadding="0" width="100%" style="width: 100%; min-width: 100%;">\n        <tr>\n          <td>\n            <p> This is a test content </p>\n            <a href="{$unsubscribe}">Unsubscribe</a>\n          </td>\n        </tr>\n      </table>\n\n    </td>\n  </tr>\n</table>\n<!-- Footer -->\n</body>\n</html>',
-      },
-    ],
-    groups: [GROUP_ID],
+    delivery: "instant",
   };
 
-  const { user } = req.body;
-  if (user === USER_ID) {
   mailerlite.campaigns
-    .create(params)
+    .schedule(CAMPAIGN_ID, params)
     .then((response) => {
-      const CAMPAIGN_ID = response.data.data.id;
-      const params = {
-        delivery: "instant",
-      };
-
-      mailerlite.campaigns
-        .schedule(CAMPAIGN_ID, params)
-        .then((response) => {
-          res.status(response.status).json(response.data);
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.error("Could not send");
-          console.log(error);
-          res.status(500).json({ error: "Error occurred" });
-        });
+      console.log(response.data);
+      res.status(response.status).json(response.data);
     })
     .catch((error) => {
       if (error.response) {
-        console.error("Error making campaign:", error);
+        console.log(error.response.data);
         res
           .status(error.response ? error.response.status : 500)
           .json({ error: "Error occurred" });
       }
     });
-  } else {
-    console.error("Unautherized user");
-        res
-          .status(500)
-          .json({ error: "Error occurred" });
-  }
 });
 
 app.listen(PORT, () => {
