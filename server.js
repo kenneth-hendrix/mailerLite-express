74a3065c8324f7b4855aa21365e14474d246d9b5
Jack Hendrix <jack@khendrix.net>
2025-03-05 23:47:49 -0500

    Added Imgur Upload Endpoint

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
 const MailerLite = require("@mailerlite/mailerlite-nodejs").default;
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const multer = require("multer");
+const axios = require("axios");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use(bodyParser.json());
 
 const API_KEY = process.env.MAILERLITE_API_KEY;
 const GROUP_ID = process.env.GROUP_ID;
+const CLIENT_ID = process.env.IMGUR_CLIENT_ID;
 
 const mailerlite = new MailerLite({
   api_key: API_KEY,
@@ -50,6 +58,45 @@ app.post("/api/subscriber", async (req, res) => {
           .json({ error: "Error occurred" });
       }
     });
+});
+
+app.post("/api/upload-image", upload.single("image"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send("No image uploaded");
+    }
+
+    const imageBuffer = req.file.buffer;
+
+    const imgurResponse = await axios.post(
+      "https://api.imgur.com/3/image",
+      imageBuffer,
+      {
+        headers: {
+          Authorization: `Client-ID ${CLIENT_ID}`,
+          "Content-Type": "application/octet-stream",
+        },
+      }
+    );
+
+    if (imgurResponse.data.success) {
+      const imgUrl = imgurResponse.data.data.link;
+
+      res.json({
+        success: true,
+        message: "Form submitted successfully!",
+        imageUrl: imgUrl,
+      });
+    } else {
+      res.status(500).send("Failed to upload image to Imgur");
+    }
+  } catch (error) {
+    console.error(
+      "Error uploading image:",
+      error.response?.data || error.message
+    );
+    res.status(500).send("Server error");
+  }
 });
 
 app.listen(PORT, () => {
--- testServer.js
+++ testServer.js
@@ -1,10 +1,16 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const multer = require("multer");
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,6 +18,10 @@ app.use(bodyParser.json());
 app.post("/api/subscriber", async (req, res) => {
   const { email } = req.body;
   res.status(200).json({ email: email });
+});
+
+app.post("/api/upload-image", upload.single("image"), async (req, res) => {
+  res.status(200).json({ success: true, imageUrl: "www.test.com" });
 });
 
 app.listen(PORT, () => {
